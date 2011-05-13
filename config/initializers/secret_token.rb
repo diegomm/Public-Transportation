a7c2ef840ad8672121a2a8b17c47364289b2034e
Diego Martinez-Moncada <diegomartinez-moncada@diego-martinez-moncadas-macbook-pro.local>
2011-05-13 11:59:42 -0500

    added all the rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublicTransportation::Application.config.secret_token = '517616ff0cf9b5a940708b1d6a5bce1411ca203916193386f6b3db8aff609a44e791331fb52f3b1eb57bb0823782b09bc92733dc46f13dc3cb4cbb10284a3451'
